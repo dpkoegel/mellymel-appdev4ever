348aa2f0a07d0324a82cc816c2f68cf46a957a15
dpkoegel <120222883+dpkoegel@users.noreply.github.com>
2022-12-10 12:39:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Favorites')
 
 streamlit.text('\U0001F44D Omega 3 & Bueberry Oatmeal')
 
-streamlit.text('\U0001F4AA	Kale, Spinach & Rocket Smoothy')
+streamlit.text('\U0001F4AA Kale, Spinach & Rocket Smoothy')
 
-streamlit.text('\U0001F9E0	Hard-Boiled Free-Range Egg')
+streamlit.text('\U0001F9E0 Hard-Boiled Free-Range Egg')
 
