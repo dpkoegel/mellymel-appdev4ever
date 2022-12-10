753f5210b7cffa8ed2b77f10eab6f0438ea05f8d
dpkoegel <120222883+dpkoegel@users.noreply.github.com>
2022-12-10 12:15:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Favorites')
 
 streamlit.text('Omega 3 & Bueberry Oatmeal')
 
-with streamlit.beta_expander('\U0001F4E1 API endpoint details')
+streamlit.beta_expander('\U0001F4E1 API endpoint details')
 streamlit.beta_expander('\U+1F44D API endpoint detail')
 streamlit.text('Kale, Spinach & Rocket Smoothy')
 streamlit.text('Hard-Boiled Free-Range Egg')
