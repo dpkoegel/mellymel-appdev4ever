9142e3634078e33568606ed90624440c7154ed3a
dpkoegel <120222883+dpkoegel@users.noreply.github.com>
2022-12-10 12:40:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ streamlit.text('\U0001F44D Omega 3 & Bueberry Oatmeal')
 
 streamlit.text('\U0001F4AA Kale, Spinach & Rocket Smoothy')
 
-streamlit.text('\U0001F9E0 Hard-Boiled Free-Range Egg')
+streamlit.text('🧠 Hard-Boiled Free-Range Egg')
 
