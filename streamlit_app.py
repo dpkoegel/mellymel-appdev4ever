b3cbeee3c231b39cda8d5d0b53838e171f4473b3
dpkoegel <120222883+dpkoegel@users.noreply.github.com>
2022-12-13 12:07:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,11 @@ streamlit.header('FruityVice Fruit Advise')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/")
+
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment 
