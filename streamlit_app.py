9908ad05b5050895636f59e5f978a403ef8f24ea
dpkoegel <120222883+dpkoegel@users.noreply.github.com>
2022-12-15 16:08:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ import pandas
 import requests
 import snowflake.connector
 
+my_cnx = snowflake.commector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select current_user(), current_account(),current_region()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake")
+streamlit.text(my_data_row)
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Favorites')
