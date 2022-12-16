eec116f9a74776bf3f792384511cafdfa5dc90c5
dpkoegel <120222883+dpkoegel@users.noreply.github.com>
2022-12-16 11:28:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import snowflake.connector
 from urllib.error import URLError
 
 # create a function
-def get_fruityvice_response (this_fruit_choice)
+def get_fruityvice_response (this_fruit_choice):
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
        return fruityvice_normalized
