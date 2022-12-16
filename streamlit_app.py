9fc576fcf1756ce1f0aed18532392a4432fbefb0
dpkoegel <120222883+dpkoegel@users.noreply.github.com>
2022-12-16 11:32:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)),['Avocado','Strawberries'])
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-#,['Avocado','Strawberries'])
+
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
@@ -33,7 +33,7 @@ try:
    if not fruit_choice:
      streamlit.error("Please select a fruit to get information")
    else:
-       back_from_function
+       back_from_function = get_fuityvice_data(fruit_choice)
        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
        #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
        # write your own comment - what does this do?
