4f4db4ebde4667b9a91833699cbb386a28f70408
dpkoegel <120222883+dpkoegel@users.noreply.github.com>
2022-12-16 10:50:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas
-import requests
+#import pandas
+#import requests
 import snowflake.connector
+from urllib.error imprt URLError
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Favorites')
